820ecb923c2c49caf84592a41640af7026583e40
DivyaBobbara <nagadivya.bobbara@narayanagroup.com>
2022-09-06 20:43:46 +0530

    added project

---
+++ PercentageGraph/BarGraph.swift
@@ -0,0 +1,13 @@
+//
+//  BarGraph.swift
+//  PercentageGraph
+//
+//  Created by Naga Divya Bobbara on 06/09/22.
+//
+
+import Foundation
+struct BarEntry{
+    let percentage : Float
+    let subjectName : String
+    let status : String
+}
---
+++ PercentageGraph/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  PercentageGraph
+//
+//  Created by Naga Divya Bobbara on 06/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let barGraphView : BarGraphView = BarGraphView()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        barGraphView.frame = CGRect(x: 30, y: 100, width: view.frame.width, height: 400)
+       
+        barGraphView.dataEntries = [
+        BarEntry(percentage: 70, subjectName: "MAT", status: "Average"),
+        BarEntry(percentage: 30, subjectName: "PHY", status: "Weak"),
+        BarEntry(percentage: 80, subjectName: "CHEM", status: "Average"),
+        BarEntry(percentage: 80, subjectName: "SCI", status: "Strong"),
+        BarEntry(percentage: 90, subjectName: "BIO", status: "Strong")
+        ]
+        
+        view.addSubview(barGraphView)
+    }
+
+
+}
+
